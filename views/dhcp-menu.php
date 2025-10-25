5c87ea3da08a63022d1133526e559f5761ed6534
Silvester van der Leer <=>
2025-10-25 08:19:02 +0200

    Update navigation: rename DHCP to DHCPv6, remove IPv6 DHCP, rename Leases to DHCPv6

--- views/dhcp-menu.php
+++ views/dhcp-menu.php
@@ -13,12 +13,9 @@
                class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium <?php echo $subPage === 'options' ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'; ?>">
                 DHCPv6 Options 
             </a>
-            <a href="/dhcp/reservations" 
-               class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium <?php echo $subPage === 'reservations' ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'; ?>">
-            </a>
             <a href="/dhcp/leases" 
                 class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium <?php echo $subPage === 'leases' ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'; ?>">
-                DHCPv6 Leases/Reservations
+                DHCPv6
             </a>
         </div>
     </div>
