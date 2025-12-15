a66e46fad9a4dad04e0c3fa6ce24b47fefee8446
Silvester van der Leer <=>
2025-12-15 18:54:31 +0100

    Send Kea commands to all servers in HA mode

--- src/Models/KeaModel.php
+++ src/Models/KeaModel.php
@@ -27,36 +27,54 @@ class KeaModel
 
     public function sendKeaCommand($command, $arguments = []) 
     {
-        // Send command to primary server (first in list)
-        $primaryServer = $this->keaServers[0];
-        
         $data = [
             "command" => $command,
             "service" => [$this->keaService],
             "arguments" => $arguments
         ];
         
-        $ch = curl_init($primaryServer['api_url']);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        // Send to all servers (HA MySQL backend requires both servers to be updated)
+        $responses = [];
+        $errors = [];
+        
+        foreach ($this->keaServers as $server) {
+            $ch = curl_init($server['api_url']);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
-        $response = curl_exec($ch);
+            $response = curl_exec($ch);
 
-        if (curl_errno($ch)) {
-            $error = curl_error($ch);
+            if (curl_errno($ch)) {
+                $error = "Kea API Error on {$server['name']}: " . curl_error($ch);
+                error_log($error);
+                $errors[] = $error;
+                curl_close($ch);
+                continue;
+            }
+            
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             curl_close($ch);
-            throw new Exception("Kea API Error ({$primaryServer['name']}): $error");
+            
+            if ($httpCode !== 200) {
+                $error = "Kea API HTTP Error on {$server['name']}: HTTP $httpCode";
+                error_log($error);
+                $errors[] = $error;
+                continue;
+            }
+            
+            $responses[] = $response;
+            error_log("Kea command '{$command}' succeeded on {$server['name']}");
         }
         
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        
-        if ($httpCode !== 200) {
-            throw new Exception("Kea API HTTP Error ({$primaryServer['name']}): HTTP $httpCode - " . substr($response, 0, 200));
+        // If ALL servers failed, throw an exception
+        if (count($errors) === count($this->keaServers)) {
+            throw new Exception("Kea command failed on all servers: " . implode("; ", $errors));
         }
-
-        return $response;
+        
+        // Return the first successful response
+        return $responses[0];
     }
 }
