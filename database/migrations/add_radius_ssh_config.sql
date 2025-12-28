e5df842115b6db70c1b566f582b53b6613eecec0
Silvester van der Leer <=>
2025-12-28 20:14:27 +0100

    Fix MySQL syntax for older versions - remove IF NOT EXISTS

--- database/migrations/add_radius_ssh_config.sql
+++ database/migrations/add_radius_ssh_config.sql
@@ -1,8 +1,9 @@
 -- Add reload flag to radius_server_config table
 -- This flag is set when NAS clients change and needs FreeRADIUS reload
 
+-- Add column (ignore error if it already exists)
 ALTER TABLE radius_server_config 
-ADD COLUMN IF NOT EXISTS needs_reload BOOLEAN DEFAULT FALSE COMMENT 'Set to TRUE when FreeRADIUS needs to reload NAS clients';
+ADD COLUMN needs_reload BOOLEAN DEFAULT FALSE COMMENT 'Set to TRUE when FreeRADIUS needs to reload NAS clients';
 
--- Create index for quick polling
-CREATE INDEX IF NOT EXISTS idx_needs_reload ON radius_server_config(needs_reload);
+-- Create index for quick polling (ignore error if it already exists)
+CREATE INDEX idx_needs_reload ON radius_server_config(needs_reload);
