bff47ad2bf552017f0da388be575829ce5c6b78f
Silvester van der Leer <=>
2025-12-28 04:06:42 +0100

    Remove MAC address column from search results

--- views/dhcp/search.php
+++ views/dhcp/search.php
@@ -219,7 +219,6 @@ ob_start();
                         <tr>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">IPv6 Address</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">DUID</th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">MAC Address</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Switch / BVI</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Valid Until</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Type</th>
@@ -495,7 +494,6 @@ function displayResults(leases, total, searchTime) {
         row.innerHTML = `
             <td class="px-6 py-4 whitespace-nowrap text-sm font-mono text-gray-900">${escapeHtml(lease.address)}</td>
             <td class="px-6 py-4 whitespace-nowrap text-sm font-mono text-gray-500">${escapeHtml(lease.duid ? lease.duid.substring(0, 20) + '...' : 'N/A')}</td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm font-mono text-gray-600">${escapeHtml(macAddress)}</td>
             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">${switchBviText}</td>
             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">${formatDate(lease.expire)}</td>
             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">${leaseType}</td>
